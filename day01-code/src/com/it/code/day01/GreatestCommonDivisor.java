32094078c8e67d3b642fc35537ddf8da511235f2
JYJ <1036470143@qq.com>
2020-05-09 23:58:08 +0800

    欧几里得算法 找到两个数的最大公约数

--- day01-code/src/com/it/code/day01/GreatestCommonDivisor.java
+++ day01-code/src/com/it/code/day01/GreatestCommonDivisor.java
@@ -24,7 +24,7 @@ public class GreatestCommonDivisor {
 
     public static int gcd(int p, int q) {
         if (q == 0) return p;
-        int r = p / q;
+        int r = p % q;
         return gcd(q, r);
     }
 }
